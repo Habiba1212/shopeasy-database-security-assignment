f6a22f1f789ee8704a82a774cd8f0414182926b8
amhussien66-png <amhussien66@gmail.com>
2026-05-23 13:20:38 +0800

    Update place_order.php

--- place_order.php
+++ place_order.php
@@ -1,7 +1,23 @@
 <?php
 session_start();
 
+// Disable browser cache
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: 0");
+
+// CUSTOMER ONLY
+if (
+    !isset($_SESSION['user_id']) ||
+    !isset($_SESSION['role']) ||
+    $_SESSION['role'] !== 'customer'
+) {
+    header("Location: login.php");
+    exit();
+}
+
 require 'db_connect.php';
+
 
 $product_id = 1;
 $quantity = 2;
