2e4d91c2346565f9886bf3558d3772084bd768c3
amhussien66-png <amhussien66@gmail.com>
2026-05-23 12:37:50 +0800

    Update driver_view.php

--- driver_view.php
+++ driver_view.php
@@ -5,14 +5,14 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 header("Expires: 0");
-require 'db_connect.php';
+
 
 // SECURITY CHECK: Allow driver only
 if (!isset($_SESSION['user_id']) || !isset($_SESSION['role']) || $_SESSION['role'] !== 'driver') {
     header("Location: login.php");
     exit();
 }
-
+require 'db_connect.php';
 
 // ---------------- START DELIVERY ACTION ----------------
 
